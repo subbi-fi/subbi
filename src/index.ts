08a8573744508b39ea859b42379843cfd4410268
Aaron <aarongkendall@gmail.com>
2022-03-17 10:19:46 +0000

    export MAX_UINT

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as SubscribeButton } from "components/SubscribeButton";
 export { default as CheckoutFlow } from "components/CheckoutFlow";
 export { default as SubbiProvider } from "context/SubbiProvider";
 export { useSubscriptionContract, useUSDCContract } from "hooks/contracts";
+export { MAX_UINT } from "config";
